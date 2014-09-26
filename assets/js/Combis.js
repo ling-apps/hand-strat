68a6a03efde59716cb1ef701f25be743d29a9030
Nicolas MEDDA <nicolas.medda@ling.fr>
2014-09-26 08:28:01 +0200

    Implement client Combi store

--- assets/js/Combis.js
+++ assets/js/Combis.js
@@ -1,11 +1,22 @@
-
+var request = require('superagent');
+var EventEmitter = require('events').EventEmitter;
 var baseUrl = '/_api/combis';
 
 function Combi(name) {
-
+  this.name = name;
 }
 
 Combi.prototype.save = function() {
+  request
+    .post(baseUrl)
+    .set('Accept', 'application/json')
+    .send({name: this.name})
+    .end(function(err, res) {
+      if (err)
+        return console.log(err);
+
+      this.id = res.body.id;
+    }.bind(this));
 };
 
 Combi.prototype.update = function() {};
@@ -14,13 +25,29 @@ Combi.prototype.destroy = function() {};
 
 // --- Store
 var _combis = [];
+var E = new EventEmitter();
 
 var Combis = {
+  getAll: function() {
+    return _combis;
+  },
 
   createCombi: function(name) {
-    _combis.push(new Combi(name));
+    var combi = new Combi(name);
+    combi.save();
+    _combis.push(combi);
+    E.emit('change');
+  },
+
+  addChangeListener: function(cb) {
+    E.on('change', cb);
+  },
+
+  removeChangeListener: function(cb) {
+    E.removeListener('change', cb);
   }
 
 };
 
 module.exports = Combis;
+
